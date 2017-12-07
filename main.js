d075c2c01f2f4b7e4c3203df32cfaa8152891ba1
SrinivasRangu <34162207+SrinivasRangu@users.noreply.github.com>
2017-12-07 14:39:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+/*var mynumber = parseInt(prompt("enter the number"));
+
+
+for(var i=1; i<=mynumber; i++){
+    
+    if(i%3==0){
+        
+       continue;
+       
+       }
+    
+    document.write("number i "+i +"<br>");
+    
+    
+}
+
+document.write("After loop"); */
+
+var mynumber = parseInt(prompt("enter the number"));
+var i="1";
+
+while( i<=mynumber){
+    
+   document.write("number i "+i +"<br>");
+    
+    i++;
+    
+    
+}
+
+document.write("After loop");
